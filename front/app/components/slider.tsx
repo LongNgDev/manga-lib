f45f48b153519146f011f3bf34ad911fe1c2e9cb
NightW0lfff <philong170101@gmail.com>
2025-12-04 04:28:30 +1100

    Feat: Add Popular manga title with carousel.

--- front/app/components/slider.tsx
+++ front/app/components/slider.tsx
@@ -8,20 +8,105 @@ import {
 	CarouselNext,
 	CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Badge } from "@/components/ui/badge";
+
+// Assets
+import bgImg from "../assets/demo.jpg";
+import Image from "next/image";
+import { Clock4, Clock4Icon } from "lucide-react";
 
 export function TopMangaSlider() {
 	return (
-		<Carousel className="w-full max-w-lg" opts={{ loop: true }}>
+		<Carousel
+			className="flex items-center w-full h-[250px]"
+			opts={{ align: "start", loop: true }}
+		>
 			<CarouselContent>
 				{Array.from({ length: 5 }).map((_, index) => (
-					<CarouselItem key={index}>
-						<div className="p-1">
-							<Card>
-								<CardContent className="flex aspect-square items-center justify-center p-6">
-									<span className="text-4xl font-semibold">{index + 1}</span>
-								</CardContent>
-							</Card>
-						</div>
+					<CarouselItem key={index} className="relative">
+						<div className="absolute top-0 left-0 w-full h-full bg-linear-to-t from-accent via-70% via-accent/75 to-accent/50 -z-10 pointer-events-none"></div>
+						<Image
+							src={bgImg}
+							alt="Background image"
+							style={{
+								objectFit: "contain",
+								width: "100%",
+								height: "auto",
+							}}
+							className="absolute top-0 left-0 -translate-y-1/6 -z-20"
+						/>
+
+						<Card className="h-[200px] border-0 shadow-none bg-accent/0 outline-0">
+							<CardContent className="flex items-center gap-2 px-2 grow">
+								{/* Thumbnail Cover Image */}
+								<Image
+									src={bgImg}
+									alt="Thumbnail image"
+									style={{
+										objectFit: "cover",
+									}}
+									className="h-full w-25 rounded-xs"
+								/>
+
+								{/* Manga Content */}
+								<div className="flex flex-col justify-between h-full">
+									<div className="flex flex-col gap-2">
+										<div className="flex flex-col gap-0.5">
+											<h2 className="font-semibold tracking-wide">
+												Uma Musume - Pretty Derby: Star Blossom
+											</h2>
+											<h3 className="text-[10px] italic">
+												ウマ娘　プリティーダービー　スターブロッサム
+											</h3>
+											<div className="flex gap-1 py-1">
+												<Badge
+													className=" text-[6px] uppercase tracking-wider py-0 px-1 font-bold"
+													variant={"destructive"}
+												>
+													Suggestive
+												</Badge>
+												<Badge
+													className=" text-[6px] uppercase tracking-wider py-0 px-1 font-bold"
+													variant={"default"}
+												>
+													Action
+												</Badge>
+												<Badge
+													className=" text-[6px] uppercase tracking-wider py-0 px-1 font-bold"
+													variant={"default"}
+												>
+													Romcom
+												</Badge>
+												<Badge
+													className=" text-[6px] uppercase tracking-wider py-0 px-1 font-bold"
+													variant={"default"}
+												>
+													Slice of Life
+												</Badge>
+											</div>
+										</div>
+										<p className="text-[9px] font-light line-clamp-3">
+											{`The popular cross-media content "Uma Musume Pretty Derby"
+											features "Uma Musume" who have inherited the names and
+											souls of numerous racehorses! A new legend begins with the
+											indomitable horse girl "Sakura Laurel" as the main
+											character.`}
+										</p>
+									</div>
+									<div className="text-[9px] flex justify-between items-baseline">
+										<span className="line-clamp-1">
+											Monjūsaki , Cygames, Hotani Shin
+										</span>
+
+										<span className="text-[8px]">
+											<Clock4 size={10} className="inline" /> 1 hour ago
+										</span>
+									</div>
+								</div>
+
+								{/* Top & Nav */}
+							</CardContent>
+						</Card>
 					</CarouselItem>
 				))}
 			</CarouselContent>
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,34 +1,19 @@
-import Image from "next/image";
 import { TopMangaSlider } from "./components/slider";
-import bgImg from "./assets/demo.jpg";
 
 export default function Home() {
 	return (
-		<div className=" w-vw min-h-vh">
-			<main className="relative w-full">
-				{/* Background blur backdrop */}
-				<div className="absolute bg-linear-to-t from-accent from-10% to-accent/50 h-full w-full top-0 left-0"></div>
+		<div className="text-xs w-vw min-h-vh">
+			<main className="relative w-full overflow-hidden">
 				{/* Navbar Section */}
-				<div className="fixed top-0 w-full bg-amber-500">Nav</div>
+				<div className="sticky top-0 w-full bg-amber-500">Nav</div>
 
 				{/* Top 10 Section */}
-				<div className="w-full h-[600px]">
+				<div className="w-full md:h-[500px] h-[200px]">
 					{/* Main Container */}
-					<div className="h-full">
-						{/* Background Image */}
-						<div className="w-full h-full">
-							<Image
-								src={bgImg}
-								alt="Background image"
-								style={{
-									objectFit: "contain",
-								}}
-								className="absolute -translate-y-1/6 -z-10"
-							/>
-						</div>
+					<div className="relative h-full grow">
 						{/* Content Container */}
-						<div className="flex justify-center w-full bg-accent">
-							{/* <TopMangaSlider /> */}
+						<div className="relative flex items-center justify-center w-full h-full">
+							<TopMangaSlider />
 						</div>
 					</div>
 				</div>
