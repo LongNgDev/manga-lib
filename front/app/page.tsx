4a28d2221f2aa0e4e6107950a48bbfdaeaf3a58d
NightW0lfff <philong170101@gmail.com>
2025-12-10 02:00:30 +1100

    feat: Transfer Recent updated cards to new component.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,13 +1,9 @@
 import { TopMangaSlider } from "./components/slider";
 
-import bgImg from "./assets/demo.jpg";
-import Image from "next/image";
-
-import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
-import { Bookmark, MoveRight, Search } from "lucide-react";
+import RecentUpdatedSection from "./components/sections/recentUpdatedSection";
 
 export default function Home() {
 	return (
@@ -47,78 +43,22 @@ export default function Home() {
 							<h2 className="absolute left-1.5 top-10 z-20 text-xs font-semibold tracking-wide">
 								Popular New Titles
 							</h2>
-							<TopMangaSlider />
+							<div className="grid grid-cols-4 row-auto gap-2">
+								{Array.from({ length: 10 }).map((_, index) => (
+									<div key={index}>
+										<TopMangaSlider />
+									</div>
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
 				{/* My Library Section */}
+
 				{/* Latest Updated Section */}
 				<div className="flex flex-col w-full gap-2 p-2 bg-accent">
 					<h2 className="font-semibold">Recent Updates</h2>
-
-					{/* List of latest manga */}
-					{/* Main Container */}
-					<div className="grid grid-cols-4 row-auto gap-2">
-						{Array.from({ length: 10 }).map((_, index) => (
-							<div key={index} className="relative w-full h-34">
-								{/* Blur backdrop */}
-								<div className="absolute z-10 w-full h-full bg-accent/95">
-									{/* Manga details */}
-									<div className="p-1 grid grid-rows-5 gap-0.5 max-h-full">
-										{/* Title */}
-										<h2 className="font-semibold tracking-wide h-fit text-[9px] line-clamp-2 ">
-											Uma Musume - Pretty Derby: Star Blossom
-										</h2>
-										{/* Alternative Title */}
-										<h3 className="text-[7px] italic line-clamp-2 h-fit">
-											ウマ娘　プリティーダービー　スターブロッサム
-										</h3>
-										{/* Chapter List */}
-										<div className="row-span-2 px-1">
-											<div className="grid row-auto h-full text-[8px] py-1">
-												{Array.from({ length: 3 }).map((_, index) => (
-													<div
-														key={index}
-														className="flex items-baseline justify-between gap-1 h-fit"
-													>
-														<h3>chapter {index + 1}</h3>
-														<div className="self-end my-0.5 border-b border-dotted grow border-accent-foreground"></div>
-														<span className="text-[6px]">50 mins ago</span>
-													</div>
-												))}
-											</div>
-										</div>
-
-										{/* CTA button */}
-										<div className="flex items-center justify-between w-full h-full">
-											<Bookmark size={15} />
-
-											<Button
-												variant={"default"}
-												className="px-1! py-0.5! rounded-sm h-fit gap-1"
-												size={"sm"}
-											>
-												<span className="text-[8px] font-semibold">
-													View details
-												</span>
-												<MoveRight className="size-2" />
-											</Button>
-										</div>
-									</div>
-								</div>
-								{/* Background Image */}
-								<Image
-									src={bgImg}
-									alt="Thumbnail Image"
-									fill
-									style={{
-										objectFit: "cover",
-									}}
-									className="absolute top-0 left-0"
-								/>
-							</div>
-						))}
-					</div>
+					<RecentUpdatedSection />
 				</div>
 			</main>
 		</div>
