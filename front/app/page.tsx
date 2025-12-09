aaaf45881eab4e2644fc08d44946bc22f6628ec2
NightW0lfff <philong170101@gmail.com>
2025-12-10 03:27:33 +1100

    feat: Add updatedCard component.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,7 +3,7 @@ import { TopMangaSlider } from "./components/slider";
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import RecentUpdatedSection from "./components/sections/recentUpdatedSection";
+import UpdatedSection from "./components/updatedCard";
 
 export default function Home() {
 	return (
@@ -11,7 +11,7 @@ export default function Home() {
 			<main className="relative w-full overflow-auto">
 				{/* Navbar Section */}
 				<div className="fixed top-0 left-0 z-10 w-full h-10">
-					<div className="flex justify-between items-center p-2 h-full">
+					<div className="flex items-center justify-between h-full p-2">
 						<div className="flex gap-1">
 							{/* <div>Logo</div> */}
 							<h1>MangaLib</h1>
@@ -22,10 +22,10 @@ export default function Home() {
 									className="h-fit w-20 focus:w-35 transition-all duration-400 ease-in-out rounded-sm text-[8px] font-semibold px-2 bg-accent/90! border-none"
 									placeholder="Search"
 								/>
-								<Search className="h-3 w-auto" />
+								<Search className="w-auto h-3" />
 							</div>
 							<div className="h-full">
-								<Avatar className="h-6 w-auto">
+								<Avatar className="w-auto h-6">
 									<AvatarImage src="https://github.com/shadcn.png" />
 									<AvatarFallback>CN</AvatarFallback>
 								</Avatar>
@@ -39,17 +39,12 @@ export default function Home() {
 					{/* Main Container */}
 					<div className="relative h-full grow">
 						{/* Content Container */}
-						<div className="relative flex  items-center justify-center w-full h-full">
+						<div className="relative flex items-center justify-center w-full h-full">
 							<h2 className="absolute left-1.5 top-10 z-20 text-xs font-semibold tracking-wide">
 								Popular New Titles
 							</h2>
-							<div className="grid grid-cols-4 row-auto gap-2">
-								{Array.from({ length: 10 }).map((_, index) => (
-									<div key={index}>
-										<TopMangaSlider />
-									</div>
-								))}
-							</div>
+
+							<TopMangaSlider />
 						</div>
 					</div>
 				</div>
@@ -58,7 +53,13 @@ export default function Home() {
 				{/* Latest Updated Section */}
 				<div className="flex flex-col w-full gap-2 p-2 bg-accent">
 					<h2 className="font-semibold">Recent Updates</h2>
-					<RecentUpdatedSection />
+					<div className="grid grid-cols-4 row-auto gap-2">
+						{Array.from({ length: 20 }).map((_, index) => (
+							<div key={index}>
+								<UpdatedSection />
+							</div>
+						))}
+					</div>
 				</div>
 			</main>
 		</div>
