6d138aa881095d30e1ac83c12d405612f567f12e
NightW0lfff <philong170101@gmail.com>
2025-12-16 01:34:42 +1100

    feat: Add fetch chapter for the worker.

--- worker/fetcher/fetcherClient.py
+++ worker/fetcher/fetcherClient.py
@@ -71,6 +71,10 @@ class Fetcher():
             if latestChapterId == getChapterId: return
 
             manga["attributes"]["latestUploadedChapterTimeStamp"] = self.__fetchLatestUploadedChapter(latestChapterId) # Assign date to this key
+
+            # Add chapter data to this key
+            manga["attributes"]["chapters"] = self.fetchChapter(manga["id"])
+            
             print(manga["attributes"]["title"], manga["attributes"]["latestUploadedChapterTimeStamp"])
             
             self.mongo_client.save(manga) # Save to the database
@@ -123,6 +127,9 @@ class Fetcher():
         # Assign date to this key
         manga["attributes"]["latestUploadedChapterTimeStamp"] = self.__fetchLatestUploadedChapter(latestChapterId) 
         
+        # Add chapter data to this key
+        manga["attributes"]["chapters"] = self.fetchChapter(manga["id"])
+        
         print(manga["attributes"]["title"], manga["attributes"]["latestUploadedChapterTimeStamp"])
         
         self.mongo_client.save(manga) # Save to the database
@@ -158,7 +165,79 @@ class Fetcher():
       print(e)
       return None
 
-  def 
+  def fetchChapter(self, mangaId):
+    # If not found any manga ID, return None
+    if not mangaId:
+      return
+    
+
+    result = []
+
+    
+    # Try to fetch data about all the chapters that are exist in this manga title
+    try:
+      res = requests.get(f"{self.base_url}/manga/{mangaId}/aggregate", timeout=10)
+
+      if not res.ok:
+        raise Exception({"status":"500", "err": res})
+      
+      # Get the volumes of the title
+      volumes = res.json()["volumes"].values()
+      
+
+      # print(volumes)
+
+      # Loop through the volume to extract the chapters
+      for volume in volumes:        
+        chapters = volume["chapters"] # List of the chapters
+        # print(chapters)
+        # Loop through the chapter from the list to extract the info of the chapter
+        for chapter_index in chapters:
+          chapter = chapters[chapter_index]
+          chapter_id = chapter["id"]
+
+          # Extract the chapter from the api to fetch the data of the chapter
+          try: 
+            res = requests.get(f"{self.base_url}/chapter/{chapter_id}", timeout=10)
+
+            if not res.ok:
+              raise Exception({"status":"500", "err": res})
+
+            chapter["data"] = res.json()["data"]
+
+            if len(chapter["others"]) > 0:
+              others = []
+
+              # Loop through the chapter in others
+              for others_id in chapter["others"]:
+                try:
+                  res = requests.get(f"{self.base_url}/chapter/{others_id}", timeout=10)
+
+                  if not res.ok:
+                    raise Exception({"status":"500", "err": res})
+                  
+                  data = res.json()["data"]
+                  others.append(data)
+
+                except Exception as e:
+                  raise Exception({"status": 500, "err" : e})
+                
+              chapter["others"] = others
+
+            # Add the data into the result
+            result.append(chapter)
+          
+          except Exception as e:
+            raise Exception({"status": 500, "err" : e})
+      
+      print(result)
+
+    except Exception as e:
+      print(f"Error: {e}")
+  
+    finally:
+      print("Done!")
+      return result
 
 
 if __name__ == "__main__":
@@ -167,5 +246,7 @@ if __name__ == "__main__":
 
   # fetcher.fetchAll())
   fetcher.fetchLatest() 
+  # fetcher.fetchChapter("c6a95695-99b2-43fa-96b1-550d40b2800f")
+  # fetcher.fetchChapter("38554690-65d0-476f-909a-9b14b3c04e60")
   # res = fetcher.fetchLatestUploadedChapter("d37be610-0a56-48c6-986e-637f4b3dee62")
   # print(res)
