9e2187bb7f665951e383601c130cbd159d1e3cf3
NightW0lfff <philong170101@gmail.com>
2025-12-24 13:24:49 +1100

    comp: Adjust the paddind for slider across the devices.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 				{/* Main Container */}
 				<div className="relative h-full grow">
 					{/* Content Container */}
-					<div className="relative flex items-center justify-center w-full h-full pt-10">
+					<div className="relative flex items-center justify-center w-full h-full">
 						<h2 className="absolute left-1.5 top-12 md:top-13 lg:top-24 z-20 text-xs md:text-lg lg:text-3xl font-semibold tracking-wide">
 							Popular New Titles
 						</h2>
